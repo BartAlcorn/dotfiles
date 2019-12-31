aceb967365d33213367abd6d55d0584952a8e073
Bart Alcorn <bart.alcorn@turner.com>
2019-12-31 08:16:13 -0500

    brew installs

--- install_brew.sh
+++ install_brew.sh
@@ -18,3 +18,43 @@ echo "Updating and upgrading Homebrew..."
 echo
 yes | brew update
 yes | brew upgrade
+
+brew install ansiweather
+brew install awscli
+brew install chtf
+brew install coreutils
+brew install cowsay
+brew install figlet
+brew install fortune
+brew install fzy
+brew install gdbm
+brew install gettext
+brew install git
+brew install glfw
+brew install go
+brew install icu4c
+brew install jq
+brew install lazydocker
+brew install libssh2
+brew install libyaml
+brew install mongodb
+brew install ncurses
+brew install oniguruma
+brew install openal-soft
+brew install openresty
+brew install openssl
+brew install pcre
+brew install pcre2
+brew install peco
+brew install pkg-config
+brew install readline
+brew install redis
+brew install rust
+brew install sqlite
+brew install starship
+brew install telnet
+brew install terraform
+brew install yarn
+brew install zsh-completions
+brew install zsh-history-substring-search
+brew install zshdb
