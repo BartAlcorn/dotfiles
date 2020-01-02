97436a9dc7d61706c9c08d5779829d672071f70c
Bart Alcorn <bart.alcorn@turner.com>
2020-01-02 13:14:18 -0500

    added wireless keyboard charge check

--- configs/mouse.zsh
+++ configs/mouse.zsh
@@ -1,7 +1,14 @@
 function mouse_battery() {
-  mouse_battery=$(ioreg -r -l -k "BatteryPercent" | grep '"BatteryPercent" =' | awk '{print $3}')
-  echo -ne $FG[cyan]$FX[bold]"Mouse: "
-  echo -ne $FG[yellow]$FX[bold]$mouse_battery
+devices=$(ioreg -r -l -k "BatteryPercent")
+
+for x in $devices; do
+  device=("${(@f)$(echo $x | grep '"Product" =' | awk '{print $4}')}")
+  charge=("${(@f)$(echo $x | grep '"BatteryPercent" =' | awk '{print $3}')}")
+done
+
+for ((i = 1; i <= $#device; i++)); do
+  echo -ne $FG[cyan]$FX[bold]"${device[i]}: "$FG[yellow]$FX[bold]${charge[i]}
   echo $FG[cyan]$FX[bold]"%"
-  echo ""
+done
+echo ""
 }
